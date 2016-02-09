6cd8e4846c417443d05e105281bbba45a1f83b31
jrd319 <jrd319@lehigh.edu>
2016-02-09 04:07:21 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("#                    #");
+        System.out.println("#  Hello World 2016! #");
+        System.out.println("#                    #");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("test");
+        
+    }    
+}
